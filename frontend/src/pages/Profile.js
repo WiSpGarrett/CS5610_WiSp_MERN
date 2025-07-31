13c7207a40c5abf3910a1b0db33cab399785c91e
WiSpGarrett <spencergarrett1997@gmail.com>
2025-07-31 15:25:06 -0400

    Added basic package.json files to root level, frontend and backend directories.  Bare minimum page structure is set up, working on Google Cloud deployment then fleshing out backend a little.

---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Profile() {
+  return (
+    <Container className="mt-4">
+      <h1>User Profile</h1>
+      <p>User authentication and profile management.</p>
+      <p>Google OAuth integration WIP</p>
+    </Container>
+  );
+}
+
+export default Profile; 
